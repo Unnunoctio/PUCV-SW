29480adeade0570d3554dced16a97775f69fe438
Rafael Carvacho <110637459+Unnunoctio@users.noreply.github.com>
2024-07-27 21:57:52 -0400

    feat: :sparkles: added enums

---
+++ src/enums.ts
@@ -0,0 +1,21 @@
+export enum Website {
+  LABORUM = 'Laborum'
+}
+
+export enum Position {
+  PERIODISTA = 'Periodista',
+  COMUNICADOR = 'Comunicador',
+  PUBLICISTA = 'Publicista',
+  RELACIONADOR_PUBLICO = 'Relacionador Público'
+}
+
+export enum Modality {
+  REMOTO = 'Remoto',
+  HIBRIDO = 'Híbrido',
+  PRESENCIAL = 'Presencial'
+}
+
+export enum Type {
+  FULL_TIME = 'Full Time',
+  PART_TIME = 'Part Time'
+}
